7369b5d26c13ce30c3e323e692642a870790be07
Kandukuri saikrishna <50510272+KANDUKURIsaikrishna@users.noreply.github.com>
2023-12-28 23:35:09 +0100

    Update index.js

--- website/index.js
+++ website/index.js
@@ -58,9 +58,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch(
-        "https://wwjcx7tyxrbjmbkf3vc3teo3mu0qrvhq.lambda-url.ca-central-1.on.aws/"
-    );
+    let response = await fetch("https://tzprobjsxh3zsnqf32rumchbby0uekdc.lambda-url.eu-north-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
 }
